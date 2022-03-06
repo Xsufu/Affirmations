5ed38fff3461962ef869a7a094b39886532a7b96
Xsufu <aluno.macarrao@gmail.com>
2022-03-06 21:39:06 +0700

    Создан и настроен RecyclerView

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+/**
+ * Адаптер для [RecyclerView] в [MainActivity]. Отображает объект данных [Affirmation]
+ */
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    /**
+     * Предоставляет ссылку на View для каждого элемента данных
+     * Каждый элемент данных - это просто Affirmation объект.
+     */
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    /**
+     * Создаёт новые View (вызывается layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // Создание новго View
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Заменяет содержимое View (вызывается layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Возвращает размер dataset
+     */
+    override fun getItemCount() = dataset.size
+}
